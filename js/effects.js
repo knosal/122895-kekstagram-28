3172409eac042a095aaa418996783506e25bd702
Knosal.V <35274816+knosal@users.noreply.github.com>
2023-04-04 23:04:24 +0300

    Правки effects.js

--- js/effects.js
+++ js/effects.js
@@ -3,7 +3,7 @@ import { imageUploadPreview } from './scale.js';
 //Массив фильтров
 const EFFECTS_FILTER = [
   { // «Оригинал»
-    name: 'none', //Эффект наложения
+    name: 'none', //Эффект наложения //или original
     style: 'none', //Функция фильтрации в свойства style
     min: 0, //min значение
     max: 100, //max значение
@@ -60,3 +60,76 @@ const effectsElement = document.querySelector('.effects');
 const effectLevelElement = document.querySelector('.effect-level__value');
 
 let currentEffect = DEFAULT_EFFECT;
+
+//Функция-проверка на эффект по умолчанию
+const isDeffaultEffect = () => currentEffect === DEFAULT_EFFECT;
+
+//Функция по скрытию слайдера
+const hideSlider = () => sliderContainer.classList.add('hidden');
+
+//Функция по показу слайдера
+const showSlider = () => sliderContainer.classList.remove('hidden');
+
+//Функция по обновлению слайдера
+const updateSlider = () => {
+  sliderElement.noUiSlider.updateOptions({
+    range: {
+      min: currentEffect.min,
+      max: currentEffect.max,
+    },
+    step: currentEffect.step,
+    start: currentEffect.max,
+  });
+
+  if (isDeffaultEffect()) {
+    hideSlider();
+  } else {
+    showSlider();
+  }
+
+  //isDeffaultEffect() ? hideSlider() : showSlider(); // не работает
+};
+
+//Обработчик эффектов
+const onEffectsChange = (evt) => {
+  if (!evt.target.classList.contains('effects__radio')) {
+    return;
+  }
+  currentEffect = EFFECTS_FILTER.find((effect) => effect.name === evt.target.value);
+  imageUploadPreview.className = `effects__preview--${currentEffect.name}`;
+  updateSlider();
+};
+
+effectsElement.addEventListener('change', onEffectsChange);
+
+//Обработчик слайдера
+const onSliderUpdate = () => {
+  const sliderValue = sliderElement.noUiSlider.get();
+  if (isDeffaultEffect()) {
+    imageUploadPreview.style.filter = DEFAULT_EFFECT.style;
+  } else {
+    imageUploadPreview.style.filter = `${currentEffect.style}(${sliderValue}${currentEffect.unit})`;
+  }
+  effectLevelElement.value = sliderValue;
+};
+
+//Функция по сбросу эффектов
+const resetEffects = () => {
+  currentEffect = DEFAULT_EFFECT;
+  updateSlider();
+};
+
+// Создание слайдера
+window.noUiSlider.create(sliderElement, {
+  range: {
+    min: DEFAULT_EFFECT.min,
+    max: DEFAULT_EFFECT.max,
+  },
+  start: DEFAULT_EFFECTS_VALUE,
+  step: DEFAULT_EFFECT.step,
+  connect: 'lower',
+});
+
+sliderElement.noUiSlider.on('update', onSliderUpdate);
+
+export { resetEffects };
